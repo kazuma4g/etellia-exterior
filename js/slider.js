cc3ff71bb36b397468029124f6d7d7882c4b6de1
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-25 22:42:58 -0400

    slider

--- js/slider.js
+++ js/slider.js
@@ -2,11 +2,11 @@ $('.button-next').click(function(){
     var curSlide = $('.slide.active');
     var nextSlide = curSlide.next();
 
-    curSlide.fadeOut(300).removeClass('active');
-    nextSlide.fadeIn(300).addClass('active');
+    curSlide.fadeOut(1).removeClass('active');
+    nextSlide.fadeIn(1).addClass('active');
 
     if(nextSlide.length == 0){
-        $('.slide').first().fadeIn(300).addClass('active');
+        $('.slide').first().fadeIn(1).addClass('active');
     }
 
     $('.slide.active').css('display','flex');
@@ -17,11 +17,11 @@ $('.button-prev').click(function(){
     var curSlide = $('.slide.active');
     var prevSlide = curSlide.prev();
 
-    curSlide.fadeOut(300).removeClass('active');
-    prevSlide.fadeIn(300).addClass('active');
+    curSlide.fadeOut(1).removeClass('active');
+    prevSlide.fadeIn(1).addClass('active');
 
     if(prevSlide.length == 0){
-        $('.slide').last().fadeIn(300).addClass('active');
+        $('.slide').last().fadeIn(1).addClass('active');
     }
 
     $('.slide.active').css('display','flex');
