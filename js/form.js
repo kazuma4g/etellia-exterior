3c01795b5a509c4e9e4344938460bd9a6baf0b5f
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-28 16:35:14 -0400

    form submit button

--- js/form.js
+++ js/form.js
@@ -1,32 +1,72 @@
-function formButtonClick(){
-    //Required
-    var inputName = $('.form-input-required.name').val();
-    var inputFurigana = $('.form-input-required.furigana').val();
-    var inputMail = $('.form-input-required.mail').val();
-    var inputInquiry = $('.form-input-required-inquiry').val();
-    //Not Required
-    var inputNumber = $('.form-input-not-required.number').val();
+$(function() {
+    $(".form-submit-btn").prop("disabled", true);
+    
+    $('.form-input-required.name').change(function(){
+        //Required
+        var inputName = $('.form-input-required.name').val();
+        var inputFurigana = $('.form-input-required.furigana').val();
+        var inputMail = $('.form-input-required.mail').val();
+        var inputInquiry = $('.form-input-required-inquiry').val();
+        
+        if( inputName.length != 0 && 
+            inputFurigana.length != 0 && 
+            inputMail.length != 0 && 
+            inputInquiry.length != 0 ){
+                $(".form-submit-btn").prop("disabled", null);
+        } else {
+            $(".form-submit-btn").prop("disabled", true);
+        }
+    });
 
-    if( inputName.length === 0 || 
-        inputFurigana.length === 0 || 
-        inputMail.length === 0 || 
-        inputInquiry.length === 0 ){
+    $('.form-input-required.furigana').change(function(){
+        //Required
+        var inputName = $('.form-input-required.name').val();
+        var inputFurigana = $('.form-input-required.furigana').val();
+        var inputMail = $('.form-input-required.mail').val();
+        var inputInquiry = $('.form-input-required-inquiry').val();
+        
+        if( inputName.length != 0 && 
+            inputFurigana.length != 0 && 
+            inputMail.length != 0 && 
+            inputInquiry.length != 0 ){
+                $(".form-submit-btn").prop("disabled", null);
+        }else {
+            $(".form-submit-btn").prop("disabled", true);
+        }
+    });
 
-        alert('必須項目に入力されていません。');
-        var form = $('.form-parent');
-        $("html, body").animate({ scrollTop: form.offset().top }, 1000);
+    $('.form-input-required.mail').change(function(){
+        //Required
+        var inputName = $('.form-input-required.name').val();
+        var inputFurigana = $('.form-input-required.furigana').val();
+        var inputMail = $('.form-input-required.mail').val();
+        var inputInquiry = $('.form-input-required-inquiry').val();
+        
+        if( inputName.length != 0 && 
+            inputFurigana.length != 0 && 
+            inputMail.length != 0 && 
+            inputInquiry.length != 0 ){
+                $(".form-submit-btn").prop("disabled", null);
+        }else {
+            $(".form-submit-btn").prop("disabled", true);
+        }
+    });
 
-    } else if( inputMail.length < 5){
-        alert('Mail address should be longer than 5 characters.');
-    } else if( inputInquiry.length < 10){
-        alert('Inquiry text should be longer than 10 characters.');
-    } else {
-        alert(
-            '名前: ' + inputName + '\n' +
-            'フリガナ: ' + inputFurigana + '\n' +
-            '電話番号: ' + inputNumber + '\n' +
-            'メールアドレス: ' + inputMail + '\n' +
-            'お問い合わせ内容: ' + inputInquiry
-            );
-    }
-}
+    $('.form-input-required-inquiry').change(function(){
+        //Required
+        var inputName = $('.form-input-required.name').val();
+        var inputFurigana = $('.form-input-required.furigana').val();
+        var inputMail = $('.form-input-required.mail').val();
+        var inputInquiry = $('.form-input-required-inquiry').val();
+        
+        if( inputName.length != 0 && 
+            inputFurigana.length != 0 && 
+            inputMail.length != 0 && 
+            inputInquiry.length != 0 ){
+                $(".form-submit-btn").prop("disabled", null);
+        }else {
+            $(".form-submit-btn").prop("disabled", true);
+        }
+    });
+
+  });
