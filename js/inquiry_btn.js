bd3cf8c96c46fd8ed3835e739a178f4498925157
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-28 06:45:22 -0400

    scroll to form

---
+++ js/inquiry_btn.js
@@ -0,0 +1,10 @@
+$('.top-image-btn').click(function(){
+    var form = $('.form-parent');
+    $("html, body").animate({ scrollTop: form.offset().top }, 1000);
+});
+
+$('.business-contents-btn').click(function(){
+    var form = $('.form-parent');
+    $("html, body").animate({ scrollTop: form.offset().top }, 1000);
+});
+
