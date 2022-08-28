26a86f56fb1e1d9595b40a1972e9e2a33c079a6d
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-28 05:54:23 -0400

    footer

--- js/form.js
+++ js/form.js
@@ -1,25 +1,3 @@
-$(document).ready(function(){
-    //Required
-    var inputName = $('.form-input-required.name').val();
-    var inputFurigana = $('.form-input-required.furigana').val();
-    var inputMail = $('.form-input-required.mail').val();
-    var inputInquiry = $('.form-input-required-inquiry').val();
-    //Not Required
-    var inputNumber = $('.form-input-not-required.number').val();
-
-    if( inputName.length === 0 || 
-        inputFurigana.length === 0 || 
-        inputMail.length < 5 || 
-        inputInquiry.length < 5 ){
-
-            $('form-submit-btn').prop('disabled', true);
-
-    } else {
-        $('form-submit-btn').prop('disabled', false);
-    }
-   
-});
-
 function formButtonClick(){
     //Required
     var inputName = $('.form-input-required.name').val();
@@ -34,7 +12,7 @@ function formButtonClick(){
         inputMail.length === 0 || 
         inputInquiry.length === 0 ){
 
-            alert('Missing text in required fields.');
+        alert('Missing text in required fields.');
 
     } else if( inputMail.length < 5){
         alert('Mail address should be longer than 5 characters.');
