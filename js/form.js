0b4516430b0d45189cc3ff6de95ef719d5e24afc
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-28 05:12:28 -0400

    form

---
+++ js/form.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+    //Required
+    var inputName = $('.form-input-required.name').val();
+    var inputFurigana = $('.form-input-required.furigana').val();
+    var inputMail = $('.form-input-required.mail').val();
+    var inputInquiry = $('.form-input-required-inquiry').val();
+    //Not Required
+    var inputNumber = $('.form-input-not-required.number').val();
+
+    if( inputName.length === 0 || 
+        inputFurigana.length === 0 || 
+        inputMail.length < 5 || 
+        inputInquiry.length < 5 ){
+
+            $('form-submit-btn').prop('disabled', true);
+
+    } else {
+        $('form-submit-btn').prop('disabled', false);
+    }
+   
+});
+
+function formButtonClick(){
+    //Required
+    var inputName = $('.form-input-required.name').val();
+    var inputFurigana = $('.form-input-required.furigana').val();
+    var inputMail = $('.form-input-required.mail').val();
+    var inputInquiry = $('.form-input-required-inquiry').val();
+    //Not Required
+    var inputNumber = $('.form-input-not-required.number').val();
+
+    if( inputName.length === 0 || 
+        inputFurigana.length === 0 || 
+        inputMail.length === 0 || 
+        inputInquiry.length === 0 ){
+
+            alert('Missing text in required fields.');
+
+    } else if( inputMail.length < 5){
+        alert('Mail address should be longer than 5 characters.');
+    } else if( inputInquiry.length < 10){
+        alert('Inquiry text should be longer than 10 characters.');
+    } else {
+        alert(
+            '名前: ' + inputName + '\n' +
+            'フリガナ: ' + inputFurigana + '\n' +
+            '電話番号: ' + inputNumber + '\n' +
+            'メールアドレス: ' + inputMail + '\n' +
+            'お問い合わせ内容: ' + inputInquiry
+            );
+    }
+}
